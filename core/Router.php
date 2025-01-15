05128d637ade3f24015568cd090774bd596ebcbb
Grizi7 <mahmoudzidan7717@std.mans.edu.eg>
2025-01-15 04:35:40 +0200

    creating views
    successfully handling views

--- core/Router.php
+++ core/Router.php
@@ -10,7 +10,7 @@
         public function __construct(Request $request){
             $this->request = $request;
         }
-        public function get(string $path, callable $callback){
+        public function get(string $path, $callback){
             $this->routes['get'][$path] = $callback;    
         }
 
@@ -18,10 +18,35 @@
             $path = $this->request->getPath();
             $method = $this->request->method();
             $callback = $this->routes[$method][$path] ?? false;
+            
             if($callback === false){
-                echo "Page not found";
-                exit;
+                return "Page not found";
             }
-            echo call_user_func($callback);
+
+            if(is_string($callback)){
+                return $this->renderView($callback);
+            }
+
+            return call_user_func($callback);
+        }
+
+        public function renderView(string $view){
+            $layoutContent = $this->layoutContent();
+            $viewContent = $this->renderOnlyView($view);
+            
+            return str_replace('{{content}}', $viewContent, $layoutContent);
+
+        }
+
+        protected function layoutContent(){
+            ob_start();
+            include_once Application::$ROOT_DIR. "/views/layouts/main.php";
+            return ob_get_clean();
+        }
+
+        protected function renderOnlyView($view){
+            ob_start();
+            include_once Application::$ROOT_DIR. "/views/$view.php";
+            return ob_get_clean();
         }
     }
--- public/index.php
+++ public/index.php
@@ -4,16 +4,10 @@
     
     use app\core\Application;
 
-    $app = new Application();
+    $app = new Application(dirname(__DIR__));
 
-    $app->router->get('/', function(){
-        return "Hello World";
-    });
-
-
-    $app->router->get('/contact', function(){
-        return "Contact";
-    });
+    $app->router->get('/home', 'home');
+    $app->router->get('/contact', 'contact');
 
 
     $app->run();
