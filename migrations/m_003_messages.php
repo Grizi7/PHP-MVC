23e58156a7a90f7ff3c6c01df91aea1981ea4d8a
Grizi7 <mahmoudzidan7717@std.mans.edu.eg>
2025-03-20 15:31:29 +0200

    Add migration for message table; define structure with fields for name, email, subject, message, status, and created_at

---
+++ migrations/m_003_messages.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+    use app\core\Application;
+    class m_003_messages {
+        public function up() {
+            
+            $db = Application::$app->db;
+            $SQL = "CREATE TABLE message (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                subject VARCHAR(255) NOT NULL,
+                message TEXT NOT NULL,
+                status TINYINT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=INNODB;";
+            $db->pdo->exec($SQL);
+        }
+
+        public function down() {
+            $db = Application::$app->db;
+            $SQL = "DROP TABLE message";
+            $db->pdo->exec($SQL);
+        }
+    }
